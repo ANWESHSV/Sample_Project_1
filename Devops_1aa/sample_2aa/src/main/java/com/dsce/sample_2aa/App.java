0adab8f4fecd2f93265148d9bfe0aed9d389857f
anwes <anwes@ANWESHSV>
2025-06-29 19:50:55 +0530

    commit

---
+++ Devops_1aa/sample_2aa/src/main/java/com/dsce/sample_2aa/App.java
@@ -0,0 +1,13 @@
+package com.dsce.sample_2aa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
